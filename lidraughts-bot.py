e1b018731220755f58bd2ef964e739028f416fd7
Archange Giscard Destiné <archangedestine@gmail.com>
2026-06-15 23:24:41 -0400

    Update lidraughts-bot.py

--- lidraughts-bot.py
+++ lidraughts-bot.py
@@ -38,7 +38,7 @@ from datetime import datetime, timezone, timedelta
 # Cutoff time: 10:40 PM ET (EDT = UTC-4)
 def is_past_challenge_cutoff():
     et_now = datetime.now(timezone(timedelta(hours=-4)))  # EDT
-    cutoff = et_now.replace(hour=22, minute=40, second=0, microsecond=0)
+    cutoff = et_now.replace(hour=23, minute=40, second=0, microsecond=0)
     return et_now >= cutoff
 
 
